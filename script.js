96f01c600a8616520d2ace76657d00788b83f781
𝓜𝓲 𝓛𝓪𝓷 <niranjansamal1999@gmail.com>
2024-06-24 00:43:05 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const BASE_URL = "https://api.exchangerate-api.com/v4/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input").value;
+  let fromCurrency = fromCurr.value;
+  let toCurrency = toCurr.value;
+
+  if (amount === "" || amount < 1) {
+    amount = 1;
+    document.querySelector(".amount input").value = "1";
+  }
+
+  const response = await fetch(`${BASE_URL}/${fromCurrency}`);
+  const data = await response.json();
+  const rate = data.rates[toCurrency];
+  const finalAmount = (amount * rate).toFixed(2);
+
+  msg.innerText = `${amount} ${fromCurrency} = ${finalAmount} ${toCurrency}`;
+};
+
+const updateFlag = (element) => {
+  const currCode = element.value;
+  const countryCode = countryList[currCode];
+  const newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  const img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
